d200e2c50e32ccf9c4826b2099d48b7c18994916
jieun chon <g471000@gmail.com>
2022-07-26 19:34:06 +0900

    palindrome checker

---
+++ ctci/src/ch_01_arrays_and_strings/Q4.java
@@ -0,0 +1,32 @@
+package ch_01_arrays_and_strings;
+
+public class Q4 {
+    public static void main(String[] args) {
+        String[] inputs = {"", "a", "Aa", "Tact Coa", "atco cta"};
+        Q4 q4 = new Q4();
+
+        for (String s : inputs) {
+            System.out.println(s + ": " + q4.isPalindrome(s));
+        }
+    }
+
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right) {
+            if (s.charAt(left) == ' ') {
+               left++;
+            } else if (s.charAt(right) == ' ') {
+                right--;
+            } else {
+                if (s.charAt(left) != s.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+}
