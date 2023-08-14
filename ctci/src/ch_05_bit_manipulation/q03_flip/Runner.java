ad263c7cb2787fcd3de2dcfd46069094d48bb7aa
Jieun Chon <g471000@gmail.com>
2023-08-14 10:56:51 +0900

    update question 3 of bit manipulation

---
+++ ctci/src/ch_05_bit_manipulation/q03_flip/Runner.java
@@ -0,0 +1,92 @@
+package ch_05_bit_manipulation.q03_flip;
+
+import java.util.ArrayList;
+
+public class Runner {
+    public static void main(String[] args) {
+        int input = 1775;
+        System.out.println(input);
+        System.out.println(FindMaxLengthByFlipABitBFS(1775));
+        System.out.println(FindMaxLengthByFlipABitOneLoop(1775));
+    }
+
+    /**
+     * O(N)
+     * @param input input integer
+     * @return max length after flip a bit
+     */
+    public static int FindMaxLengthByFlipABitOneLoop(int input) {
+        String binaryString = Integer.toBinaryString(input);
+        int binLen = binaryString.length();
+        ArrayList<Integer> lenList = new ArrayList<>();
+
+        int currLen = 0;
+        for (int i = 0; i < binLen; i++) {
+            if (binaryString.charAt(i) == '1') {
+                currLen++;
+                if (i == binLen - 1) {
+                    lenList.add(currLen);
+                }
+            } else {
+                if (currLen > 0) {
+                    lenList.add(currLen);
+                    currLen = 0;
+                }
+            }
+        }
+
+        int maxLength = 0;
+        int left = 0, right = 1;
+        while (right < lenList.size()) {
+            maxLength = Math.max(maxLength, lenList.get(left) + lenList.get(right));
+            left++;
+            right++;
+        }
+
+        return maxLength;
+    }
+
+    /**
+     * O(N^2)
+     * @param input input integer
+     * @return max length after flip a bit
+     */
+    public static int FindMaxLengthByFlipABitBFS(int input) {
+        String binaryString = Integer.toBinaryString(input);
+        int binLen = binaryString.length();
+        if (binaryString.length() < 2) {
+            return binaryString.length();
+        }
+
+        int left = 0;
+        int right = 0;
+        int zeroIndex = -1;
+        int maxLength = 0;
+        boolean hasMetZero = false;
+
+
+        System.out.println(binaryString);
+
+        while (left < binLen) {
+            if (right == binLen - 1) {
+                maxLength = Math.max(maxLength, right - left);
+                break;
+            } else if (binaryString.charAt(right) == '0') {
+                if (hasMetZero) {
+                    maxLength = Math.max(maxLength, right - left);
+                    left = zeroIndex + 1;
+                    right = left;
+                    hasMetZero = false;
+                } else {
+                    hasMetZero = true;
+                    zeroIndex = right;
+                }
+            }
+            right++;
+        }
+
+        maxLength = Math.max(maxLength, right - left);
+
+        return maxLength;
+    }
+}
